2388c206fd0382b40bccb7c436b082942ff07c52
Dominik Forster <dominik.forster@axa.ch>
2026-03-19 10:47:52 +0100

    feat: :sparkles: update tab name to 11123

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1112</IonLabel>
+            <IonLabel>Tab 11123</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
