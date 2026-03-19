bf2d4749196cc2a499dad659987f4d9ae29be2fa
Dominik Forster <dominik.forster@axa.ch>
2026-03-19 11:45:57 +0100

    fix: :bug: update tab name to 1001

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1000</IonLabel>
+            <IonLabel>Tab 1001</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
